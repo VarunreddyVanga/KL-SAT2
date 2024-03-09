2a7cc9ef5855dfeb1801c96bc7f5436f3fd3feb5
Srivarun Reddy Vanga <159563252+VarunreddyVanga@users.noreply.github.com>
2024-03-09 17:14:22 +0530

    Update api_proxy.php

--- api_proxy.php
+++ api_proxy.php
@@ -3,7 +3,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
 // Replace YOUR_API_KEY with your actual API key
-$apiUrl = "https://api.aprs.fi/api/get?name=VU2LWI-12&what=loc&apikey=YOUR_API_KEY&format=json";
+$apiUrl = "https://api.aprs.fi/api/get?name=VU2LWI-12&what=loc&apikey=194964.xPjRJblFC7JwIN16&format=json";
 
 // Make the request to the external API
 $response = file_get_contents($apiUrl);
