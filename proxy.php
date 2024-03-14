0389ddc12d190666360a2f32184fa71a4c17ef22
srivarunreddyv <Srivarunreddyv@gmail.com>
2024-03-14 12:48:22 +0530

    delete

--- proxy.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: text/csv');
-
-$sheetURL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRLRnCBrA9vF7iBYoYy5auhrtM2f2xBuLngC9e9gd8uquoAdDO6W2b3eOuYBwTGLU0DkrfZIweQ4NDK/pub?gid=1&single=true&output=csv";
-$data = file_get_contents($sheetURL);
-
-echo $data;
-?>
